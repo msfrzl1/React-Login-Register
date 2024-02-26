c48a3c17e405b892606bf00e0cb7ed708b1768c0
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-02-26 20:44:47 +0700

    Menghubungkan router dari app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import UserPage from "./pages/UserPage";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/user" element={<UserPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
